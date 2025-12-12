e6a1493f28b45e469ad99b9794005daaab20e0e4
Brian <brian.rodrigues@silabs.com>
2025-12-12 17:34:29 +0100

    Fixed bootloader map, runtime checked. TX crashes

--- autogen/gatt_db.h
+++ autogen/gatt_db.h
@@ -20,8 +20,6 @@ extern const sli_bt_gattdb_t gattdb;
 #define gattdb_hardware_revision_string       20
 #define gattdb_firmware_revision_string       22
 #define gattdb_system_id                      24
-#define gattdb_ota                            25
-#define gattdb_ota_control                    27
 
 #define gattdb_generic_attribute_len          2
 #define gattdb_service_changed_char_len       4
@@ -34,7 +32,6 @@ extern const sli_bt_gattdb_t gattdb;
 #define gattdb_hardware_revision_string_len   5
 #define gattdb_firmware_revision_string_len   8
 #define gattdb_system_id_len                  8
-#define gattdb_ota_len                        16
 
 
 #endif // __GATT_DB_H
